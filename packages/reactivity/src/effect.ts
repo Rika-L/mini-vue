02052a531c143c2efcd77031e0d375d88ee8b701
Rika-L <2209393334@qq.com>
2025-01-02 15:21:29 +0800

    feat: 依赖收集

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -11,6 +11,9 @@ export function effect(fn, options?) {
 export let activeEffect
 
 class ReactiveEffect {
+  _trackId = 0 // 用于记录当前effect执行了几次
+  deps = [] // 用于存放effect中用到的属性
+  _depsLength = 0 // 用于记录deps的长度
   public active = true // 创建的effect是响应式的
   // fn 用户编写的函数
   // 如果fn依赖的数据变化后，需要重新调用scheduler
@@ -30,6 +33,20 @@ class ReactiveEffect {
     }
     finally {
       activeEffect = lastEffect
+    }
+  }
+}
+
+export function trackEffect(effect, dep) {
+  dep.set(effect, effect._trackId)
+  // 让effect和dep关联起来
+  effect.deps[effect._depsLength++] = dep
+}
+
+export function triggerEffects(dep) {
+  for (const effect of dep.keys()) {
+    if (effect.scheduler) {
+      effect.scheduler() // => effect.run
     }
   }
 }
--- packages/reactivity/src/reactiveEffect.ts
+++ packages/reactivity/src/reactiveEffect.ts
@@ -1,12 +1,53 @@
-import { activeEffect } from './effect'
+import { activeEffect, trackEffect, triggerEffects } from './effect'
 
+export function createDep(cleanup, key) {
+  const dep = new Map() as any
+  dep.cleanup = cleanup
+  dep.name = key
+  return dep
+}
+
+const targetMap = new WeakMap() // 存放依赖收集的关系
 export function track(target, key) {
   // activeEffect 有这个属性说明这个key是在effect中使用的
   // 如果没有这个属性说明这个key是在effect之外使用的
   // 不用进行收集
+  if (activeEffect) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) { // 如果是新增的依赖,先创建一个新map
+      targetMap.set(target, (depsMap = new Map()))
+    }
+    let dep = depsMap.get(key)
+    if (!dep) {
+      depsMap.set(key, dep = createDep(() => {
+        depsMap.delete(key)
+      }, key)) // 后面用于清理不需要的属性
+    }
+    trackEffect(activeEffect, dep) // 将当前的effect放入dep中。后续可以根据值得变化触发此dep中存放的effect
+  }
+}
 
-  if (activeEffect === undefined) {
+export function trigger(target, key, newValue, oldValue) {
+  const depsMap = targetMap.get(target)
+  if (!depsMap) { // 找不到对象，直接return即可
     return
   }
-  console.log('track', key)
+  const dep = depsMap.get(key)
+  if (dep) {
+    // 修改的属性对应了effect
+    triggerEffects(dep)
+  }
 }
+
+// Map
+// {
+//   {name:'rika',age:18}:{
+//     age:{
+//       effect
+//     },
+//     name:{
+//       effect,
+//       effect
+//     }
+//   }
+// }
