160a98c1c0b16bcd7e7982d750c38183f8d0089d
lex <sx_lex@canway.net>
2025-06-09 16:00:21 +0800

    修改测试

--- packages/reactivity/__tests__/effect.spec.ts
+++ packages/reactivity/__tests__/effect.spec.ts
@@ -90,31 +90,31 @@ describe('effect', () => {
     expect(dummy).toBe(2);
   });
 
-   it("stop", () => {
-    let dummy;
-    const obj = reactive({ prop: 1 });
-    const runner = effect(() => {
-      dummy = obj.prop;
-    });
-    obj.prop = 2;
-    expect(dummy).toBe(2);
-    stop(runner);
-    // obj.prop = 3
-    obj.prop++;
-    expect(dummy).toBe(2);
+  //  it("stop", () => {
+  //   let dummy;
+  //   const obj = reactive({ prop: 1 });
+  //   const runner = effect(() => {
+  //     dummy = obj.prop;
+  //   });
+  //   obj.prop = 2;
+  //   expect(dummy).toBe(2);
+  //   stop(runner);
+  //   // obj.prop = 3
+  //   obj.prop++;
+  //   expect(dummy).toBe(2);
 
-    // stopped effect should still be manually callable
-    runner();
-    expect(dummy).toBe(3);
-  }); 
+  //   // stopped effect should still be manually callable
+  //   runner();
+  //   expect(dummy).toBe(3);
+  // }); 
 
-  it("events: onStop", () => {
-    const onStop = vi.fn();
-    const runner = effect(() => {}, {
-      onStop,
-    });
+  // it("events: onStop", () => {
+  //   const onStop = vi.fn();
+  //   const runner = effect(() => {}, {
+  //     onStop,
+  //   });
 
-    stop(runner);
-    expect(onStop).toHaveBeenCalled();
-  });
+  //   stop(runner);
+  //   expect(onStop).toHaveBeenCalled();
+  // });
 });
