76d1ef16bd42f5ced96e0fbba53601c6303d565e
lex <sx_lex@canway.net>
2025-06-10 16:45:12 +0800

    watch 优化 watchEffect

--- packages/reactivity/__tests__/apiWatch.spec.ts
+++ packages/reactivity/__tests__/apiWatch.spec.ts
@@ -1,4 +1,4 @@
-import { reactive, watch } from "@vue/reactivity";
+import { reactive, ref, watch, watchEffect } from "@vue/reactivity";
 
 describe("api: watch", () => {
   it("happy path", () => {
@@ -6,6 +6,48 @@ describe("api: watch", () => {
     let dummy = 0;
     watch(state, (newValue) => {
       dummy = newValue.count;
+    });
+    expect(dummy).toBe(0);
+    state.count++;
+    expect(dummy).toBe(1);
+  });
+
+  it('should handle ref', () => {
+    const state = ref(0);
+    let dummy = 0;
+    watch(state, (newValue) => {
+      dummy = newValue;
+    });
+    expect(dummy).toBe(0);
+    state.value++;
+    expect(dummy).toBe(1);
+  });
+
+  it('should handle function', () => {
+    const state = reactive({ count: 0 });
+    let dummy = 0;
+    watch(() => state.count, (newValue) => {
+      dummy = newValue;
+    });
+    expect(dummy).toBe(0);
+    state.count++;
+    expect(dummy).toBe(1);
+  });
+
+  it('show handle immediate option', () => {
+    const state = reactive({ count: 0 });
+    let dummy = -1;
+    watch(state, (newValue) => {
+      dummy = newValue.count;
+    }, { immediate: true });
+    expect(dummy).toBe(0);
+  });
+
+  it('watchEffect', () => {
+    const state = reactive({ count: 0 });
+    let dummy = 0;
+    watchEffect(() => {
+      dummy = state.count;
     });
     expect(dummy).toBe(0);
     state.count++;
--- packages/reactivity/src/apiWatch.ts
+++ packages/reactivity/src/apiWatch.ts
@@ -1,10 +1,16 @@
 // 本来watch应该在runtime-core下的 现在写在这边
 
-import { isObject } from "@vue/shared";
+import { isFunction, isObject } from "@vue/shared";
 import { ReactiveEffect } from "./effect";
+import { isReactive } from "./reactive";
+import { isRef } from "./ref";
 
 export function watch(source, cb, options = {} as any) {
   return doWatch(source, cb, options);
+}
+
+export function watchEffect(source, options = {} as any) {
+  return doWatch(source, null, options);
 }
 
 // 控制depth已经当前遍历到了哪一层
@@ -27,22 +33,44 @@ function traverse(source, depth, currentDepth = 0, seen = new Set()) {
   return source;
 }
 
-function doWatch(source, cb, { deep }) {
+function doWatch(source, cb, { deep, immediate } = {} as any) {
+  console.log(isFunction(source));
   // source => getter?
   // 产生一个可以给ReactiveEffect来使用的getter 需要对这个对象进行取值操作 会关联当前的reactiveEffect
   const reactiveGetter = (source) =>
     traverse(source, deep === false ? 1 : undefined);
-  let getter = () => reactiveGetter(source);
+  let getter;
+  if (isReactive(source)) {
+    // 是响应式对象
+    getter = () => reactiveGetter(source);
+  } else if (isRef(source)) {
+    getter = () => source.value;
+  } else if (isFunction(source)) {
+    getter = source;
+  }
   let oldValue;
 
   const job = () => {
-    const newValue = effect.run();
-    cb(newValue, oldValue);
-    oldValue = newValue;
+    if (cb) {
+      const newValue = effect.run();
+      cb(newValue, oldValue);
+      oldValue = newValue;
+    }else{
+      effect.run();
+    }
   };
 
   const effect = new ReactiveEffect(getter, job);
 
-  oldValue = effect.run();
-
+  if (cb) {
+    if (immediate) {
+      // 立即执行一次用户的回调
+      job();
+    } else {
+      oldValue = effect.run();
+    }
+  } else {
+    // watchEffect
+    effect.run(); // 直接执行即可
+  }
 }
