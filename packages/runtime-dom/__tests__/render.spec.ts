b066eb5389a69a53a7b8d5880883637335f93436
lex <sx_lex@canway.net>
2025-06-12 22:12:03 +0800

    render

---
+++ packages/runtime-dom/__tests__/render.spec.ts
@@ -0,0 +1,25 @@
+import { h } from "../../../public/vue.esm-browser.js";
+import { render } from "@vue/runtime-dom";
+
+describe("runtime-core: render", () => {
+  it("happy path", () => {
+    const vnode = h("div", {}, "hi");
+    const container = document.createElement("div");
+    render(vnode, container);
+    expect(container.innerHTML).toBe("<div>hi</div>");
+  });
+
+  it("should handle array children", () => {
+    const vnode = h("div", [h("p", "hello"), h("p", "world")]);
+    const container = document.createElement("div");
+    render(vnode, container);
+    expect(container.innerHTML).toBe("<div><p>hello</p><p>world</p></div>");
+  });
+
+  it.skip("should handle have string in array children", ()=>{
+    const vnode = h("div", [h("p", "hello"), "world"]);
+    const container = document.createElement("div");
+    render(vnode, container);
+    expect(container.innerHTML).toBe("<div><p>hello</p>world</div>");
+  })
+});
--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,8 +1,12 @@
-export * from "@vue/reactivity";
-
 import { nodeOps } from "./nodeOps";
 import patchProp from "./patchProp";
+import { createRenderer } from "@vue/runtime-core";
 
 // 将节点操作和属性操作合并
 const renderOptions = Object.assign({ patchProp }, nodeOps);
-function createRenderer() {}
+
+export const render = (vnode, container) => {
+  return createRenderer(renderOptions).render(vnode, container)
+}
+
+export * from "@vue/runtime-core";
