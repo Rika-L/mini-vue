815ff5b35e53edaa9356c3fbc56d563322f91d3e
lex <sx_lex@canway.net>
2025-06-21 14:26:36 +0800

    github page

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,27 +2,27 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "mini-vue",
-  description: "A VitePress Site",
+  title: 'mini-vue',
+  description: '基于Vue3.4实现最简的Vue3模型',
+  base: '/mini-vue/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'Reactivity',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'effect', link: '/effect' },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
 })
